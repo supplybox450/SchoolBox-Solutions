1792862152ef9a0cfe27075dbb9357b397abbb4e
supplybox450 <info@supplycollectivesbox.com>
2026-03-22 00:37:23 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/SchoolBox-Solutions/',
   server: {
     host: "::",
     port: 8080,
